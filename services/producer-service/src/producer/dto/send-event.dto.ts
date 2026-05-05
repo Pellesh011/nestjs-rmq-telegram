93d0df9c1824a7ca4f2f8b38a9bd9da44b976434
vitalii <pellesh011@gmail.com>
2026-05-05 14:49:00 +0300

    refactor: swagger examples

--- services/producer-service/src/producer/dto/send-event.dto.ts
+++ services/producer-service/src/producer/dto/send-event.dto.ts
@@ -1,11 +1,19 @@
-// send-event.dto.ts
-import { IsString, IsNotEmpty, IsObject, IsOptional } from 'class-validator';
+import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class SendEventDto {
+  @ApiProperty({
+    example: 'user.created',
+    description: 'Тип события',
+  })
   @IsString()
   @IsNotEmpty()
   type!: string;
 
+  @ApiPropertyOptional({
+    example: '{"id":1}',
+    description: 'Payload события',
+  })
   @IsString()
   @IsOptional()
   payload?: string;
